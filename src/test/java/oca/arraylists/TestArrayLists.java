5349a7217a48e764a7aea8a84d7e1e788e99aee8
FLI\nbaynham <nbaynham@flhi.com>
2019-02-18 13:48:45 -0500

    ArrayList methods on exam

--- src/test/java/oca/arraylists/TestArrayLists.java
+++ src/test/java/oca/arraylists/TestArrayLists.java
@@ -17,5 +17,14 @@ public class TestArrayLists {
         System.out.println(c + " " + index);
         c.add(index, "Berlin");
         System.out.println(c + " " + c.indexOf("Berlin"));
+        c.clear();
+        c.add("z");
+        c.add(0,"x");
+        c.add(1, "y");
+        System.out.println(c);
+        if (c.contains("x")) System.out.println("C contains X");
+        c.remove(0);
+        if (! c.contains("x")) System.out.println("C no longer contains X");
+
     }
 }
